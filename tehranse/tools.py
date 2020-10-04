9c67e4ba1541ee667cb6bab7503fcceebc42e418
SinaMobasheri <sinamobasheri@outlook.com>
2020-10-04 13:37:13 +0330

    add impactindex function to tools module

--- tehranse/tools.py
+++ tehranse/tools.py
@@ -36,6 +36,44 @@ def csvwriter(filename, dictionary):
 
             write.writerow([key] + [value for value in dictionary[key].values()])
 
+def impactindex(many, flow):
+
+    from re import findall
+    from bs4 import BeautifulSoup
+    from requests import get
+
+    url = "http://tsetmc.com/Loader.aspx"
+    payload = {"Partree": "151316", "Flow": flow}
+
+    htmlfile = get(url, params=payload)
+    htmlfile = htmlfile.text
+
+    soup = BeautifulSoup(htmlfile, "html.parser")
+    tbody = soup.find("tbody")
+    trs = tbody.find_all("tr")
+
+    pack = {}
+    for tr in trs[:many]:
+
+        tds = tr.find_all("td")
+
+        inscode = findall(r"i=(.+)", tr.find("a").get("href"))[0]
+        sharename = tds[0].string
+        companyname = tds[1].string
+        pc = int(tds[2].string.replace(",", ""))
+        impact = -1*float(tds[3].string.replace("(", "").replace(")", "")) if "(" in tds[3].string else float(tds[3].string)
+
+        pack.update({
+            inscode: {
+                "sharename": sharename,
+                "companyname": companyname,
+                "pc": pc,
+                "impact": impact
+            }
+        })
+
+    return pack
+
 if __name__ == "__main__":
 
     pass
